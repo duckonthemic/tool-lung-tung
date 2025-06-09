c926707ab0dba6470d5ae980f8ef44d3d9d7edc7
Daddy iz cuming!! <118231487+duckonthemic@users.noreply.github.com>
2025-06-09 10:50:46 +0700

    Update tool-grid.bat

--- tool-grid.bat
+++ tool-grid.bat
@@ -1,108 +1,95 @@
 @echo off
-setlocal
+:: AutoSort 2.0 – File Organizer & Logger
+:: Usage :  autosort.bat  [src]  [dst]  [/copy]  [/quiet]
+:: Author : DK & ChatGPT – 09-Jun-2025
 
-:: Store the name of the current .bat file to exclude it from sorting
-set "current_bat=%~nx0"
-set "logfile=%~dp0log.txt"
+:: ---------- 1. Chuẩn bị môi trường ----------
+setlocal enableextensions enabledelayedexpansion
+set "MYNAME=%~nx0"
 
-:: Delete old log (if exists) and create a new log file
-echo *** Sorting Log - %date% %time% *** > "%logfile%"
+:: Xử lý tham số
+set "SRC=%~1"
+if "%SRC%"=="" set "SRC=%CD%"
+if not exist "%SRC%" (
+    echo [ERR] Source "%SRC%" not found & exit /b 1
+)
+pushd "%SRC%" >nul
 
-:: Create folders if they don't already exist
-mkdir "Document"
-mkdir "Img"
-mkdir "Exe-F"
-mkdir "Sound"
-mkdir "Zipped"
-mkdir "Non-detect"
+set "DST=%~2"
+if "%DST%"=="" set "DST=%SRC%"
 
-:: Display start message
-echo Starting file sorting... Please wait.
-set /a doc_count=0 & set /a img_count=0 & set /a exe_count=0 & set /a sound_count=0 & set /a zipped_count=0 & set /a non_detect_count=0
+set "MODE=move"
+if /i "%~3"=="/copy"    set "MODE=copy"
+if /i "%~4"=="/copy"    set "MODE=copy"
+if /i "%~3"=="/quiet"   set "QUIET=1"
+if /i "%~4"=="/quiet"   set "QUIET=1"
 
-:: Simple loading animation
-call :loading_animation & echo.
-
-:: Move document files
-for %%f in (*.txt *.pdf *.doc *.docx *.xls *.xlsx *.ppt *.pptx) do (
-    if /i not "%%~nxf"=="%current_bat%" move /Y "%%f" "Document" >nul 2>&1 && set /a doc_count+=1
+:: File log theo ngày-giờ
+for /f %%t in ("%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%") do (
+    set "LOG=%DST%\sort_%%t.log"
 )
 
-:: Move image files
-for %%f in (*.jpg *.jpeg *.png *.gif *.bmp *.tif *.tiff) do (
-    if /i not "%%~nxf"=="%current_bat%" move /Y "%%f" "Img" >nul 2>&1 && set /a img_count+=1
+:: ---------- 2. Bảng danh mục ----------
+::   cú pháp:  call :addCat "FolderName" "ext1 ext2 ext3"
+set "_CAT_COUNT=0"
+call :addCat "Document"   "txt doc docx pdf xls xlsx ppt pptx csv odt ods odp"
+call :addCat "Image"      "jpg jpeg png gif bmp tif tiff webp svg"
+call :addCat "Audio"      "mp3 wav flac aac ogg m4a"
+call :addCat "Video"      "mp4 mkv mov avi wmv webm"
+call :addCat "Archive"    "zip rar 7z tar gz bz2 iso"
+call :addCat "Executable" "exe msi bat cmd ps1"
+call :addCat "Script"     "js py rb vbs sh php"
+call :addCat "Other"      ""   :: phải đặt cuối
+
+:: ---------- 3. Vòng lặp duy nhất ----------
+if not defined QUIET echo Sorting files from "%SRC%" to "%DST%"...
+> "%LOG%" (
+    echo === AutoSort 2.0  –  %date% %time%
 )
 
-:: Move executable files
-for %%f in (*.exe *.cmd *.msi) do (
-    if /i not "%%~nxf"=="%current_bat%" move /Y "%%f" "Exe-F" >nul 2>&1 && set /a exe_count+=1
-)
-
-:: Move audio files
-for %%f in (*.mp3 *.wav *.flac *.aac *.ogg) do (
-    if /i not "%%~nxf"=="%current_bat%" move /Y "%%f" "Sound" >nul 2>&1 && set /a sound_count+=1
-)
-
-:: Move compressed files
-for %%f in (*.zip *.rar *.7z *.tar *.gz) do (
-    if /i not "%%~nxf"=="%current_bat%" move /Y "%%f" "Zipped" >nul 2>&1 && set /a zipped_count+=1
-)
-
-:: Move unidentified files
-for %%f in (*) do (
-    if /i not "%%~nxf"=="%current_bat%" (
-        if not exist "Document\%%~nxf" (
-            if not exist "Img\%%~nxf" (
-                if not exist "Exe-F\%%~nxf" (
-                    if not exist "Sound\%%~nxf" (
-                        if not exist "Zipped\%%~nxf" (
-                            move /Y "%%f" "Non-detect" >nul 2>&1 && set /a non_detect_count+=1
-                        )
-                    )
-                )
+for %%F in (*.*) do (
+    if /i not "%%~nxF"=="%MYNAME%" (
+        set "DESTFOLDER="
+        for /l %%I in (1,1,%_CAT_COUNT%) do (
+            for %%E in (!CAT%%I_EXT!) do (
+                if /i "%%~xF"==".%%E" set "DESTFOLDER=!CAT%%I_DIR!"
             )
+        )
+        if not defined DESTFOLDER set "DESTFOLDER=!CAT%_CAT_COUNT%_DIR!"  :: Other
+
+        if not exist "%DST%\!DESTFOLDER!" mkdir "%DST%\!DESTFOLDER!" >nul
+
+        %MODE% /y "%%~fF" "%DST%\!DESTFOLDER!\"
+        if !ERRORLEVEL! == 0 (
+            echo [%%~nxF]  →  !DESTFOLDER!>>"%LOG%"
+            set /a CNT!DESTFOLDER!+=1
         )
     )
 )
 
-:: Log results
-echo Documents: %doc_count% >> "%logfile%"
-echo Images: %img_count% >> "%logfile%"
-echo Executables: %exe_count% >> "%logfile%"
-echo Audio files: %sound_count% >> "%logfile%"
-echo Compressed files: %zipped_count% >> "%logfile%"
-echo Unidentified files: %non_detect_count% >> "%logfile%"
-
-:: Display results
-echo.
-echo ---------------------
-echo Sorting complete!
-echo Documents: %doc_count%
-echo Images: %img_count%
-echo Executables: %exe_count%
-echo Audio files: %sound_count%
-echo Compressed files: %zipped_count%
-echo Unidentified files: %non_detect_count%
-echo ---------------------
-echo Log saved to log.txt
-echo.
-
-:: Play completion sound (if supported)
-echo ^G
-
-pause
-exit
-
-:: Loading animation function
-:loading_animation
-setlocal enabledelayedexpansion
-for /l %%i in (1,1,3) do (
-    set /p "=Sorting files." <nul
-    ping -n 2 localhost >nul
-    set /p "=." <nul
-    ping -n 2 localhost >nul
-    set /p "=." <nul
-    ping -n 2 localhost >nul
-    echo.
+:: ---------- 4. Kết quả ----------
+echo.>>"%LOG%"
+for /l %%I in (1,1,%_CAT_COUNT%) do (
+    call :showCnt !CAT%%I_DIR!
 )
+
+if not defined QUIET (
+    echo ------------- Summary -------------
+    type "%LOG%" | more
+    echo Log saved: "%LOG%"
+)
+popd >nul & endlocal & goto :eof
+
+:: ********** 5. Hàm & Thủ tục **********
+:addCat
+set /a _CAT_COUNT+=1
+set "CAT%_CAT_COUNT%_DIR=%~1"
+set "CAT%_CAT_COUNT%_EXT=%~2"
+set "CNT%~1=0"
+exit /b
+
+:showCnt
+set "TMPCNT=!CNT%1!"
+if "%TMPCNT%"=="" set "TMPCNT=0"
+echo %1: %TMPCNT%>>"%LOG%"
 exit /b
